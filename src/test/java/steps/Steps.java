c8f76749501e3695be8a9ce3cacaaa7517d3a9c1
nijat <nijatmn@gmail.com>
2019-07-18 20:47:33 -0400

    added 20 tc

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -12,10 +12,33 @@ public class Steps {
 
     private WebDriver driver;
 
+    @Given("^user opened google chrome browser$")
+    public void user_opened_google_chrome_browser() {
+        String driverPath = System.getProperty("user.dir") + "/src/test/resource/drivers/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+    }
 
+    @When("^he types Yahoo Japan at google search enginess$")
+    public void he_types_at_google_search_engine()  {
+        driver.findElement(By.name("q")).sendKeys("what is Functional Test");
+        pause();
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        System.out.println("I HAVE ADDED 20 Test cases");
+    }
 
+    @Then("^he should be able to visit to yahoo japan website$")
+    public void he_should_be_able_to_visit_to_yahoo_japan_website(){
+        driver.findElement(By.xpath("(//h3)[1]")).click();
+    }
 
-
+    @When("^he types query at google search$")
+    public void he_types_query_at_google_search() throws Throwable {
+        driver.findElement(By.name("q")).sendKeys("what is Regression Test");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    }
 
     public static void pause() {
         try {
